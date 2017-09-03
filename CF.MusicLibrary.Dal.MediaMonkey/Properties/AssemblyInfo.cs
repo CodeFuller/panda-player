d26c7c479fe94f69620eca829336f02efea2a504
CodeFuller <CodeFuller@gmail.com>
2017-09-03 21:02:50 +0300

    Remove MeidaMonkey projects from solution

--- CF.MusicLibrary.Dal.MediaMonkey/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CF.MusicLibrary.Dal.MediaMonkey")]
-[assembly: AssemblyDescription("This assembly contains MediaMonkey Data Access Layer of CF.MusicLibrary")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("733319e0-d824-4fcf-99a8-2f1592a6d26c")]
