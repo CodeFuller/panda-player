d0ea48d633ad097dab0d5001909980605b790960
CodeFuller <CodeFuller@gmail.com>
2017-09-14 21:43:01 +0300

    Add auto-scrolling to bottom of log view when new message is logged

--- CF.MusicLibrary.PandaPlayer/Views/LoggerView.xaml.cs
+++ CF.MusicLibrary.PandaPlayer/Views/LoggerView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using CF.MusicLibrary.PandaPlayer.ViewModels;
 
 namespace CF.MusicLibrary.PandaPlayer.Views
 {
@@ -10,6 +13,22 @@ namespace CF.MusicLibrary.PandaPlayer.Views
 		public LoggerView()
 		{
 			InitializeComponent();
+		}
+
+		private void View_Loaded(object sender, RoutedEventArgs e)
+		{
+			var viewModel = DataContext as LoggerViewModel;
+			if (viewModel != null)
+			{
+				viewModel.Messages.CollectionChanged += (s, ev) => ScrollToDataGridBottom(MessagesDataGrid);
+			}
+		}
+
+		private static void ScrollToDataGridBottom(DataGrid dataGrid)
+		{
+			var border = VisualTreeHelper.GetChild(dataGrid, 0) as Decorator;
+			var scroll = border?.Child as ScrollViewer;
+			scroll?.ScrollToEnd();
 		}
 	}
 }
