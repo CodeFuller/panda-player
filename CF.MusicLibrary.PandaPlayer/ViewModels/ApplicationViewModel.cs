66fac536221497e3f059ea3ab0ea4c80e8f912da
CodeFuller <CodeFuller@gmail.com>
2017-09-14 18:15:40 +0300

    Fix duplicated call of ApplicationViewModel.OnPlayDiscLaunched()

--- CF.MusicLibrary.PandaPlayer/ViewModels/ApplicationViewModel.cs
+++ CF.MusicLibrary.PandaPlayer/ViewModels/ApplicationViewModel.cs
@@ -72,8 +72,6 @@ namespace CF.MusicLibrary.PandaPlayer.ViewModels
 			Messenger.Default.Register<PlayDiscEventArgs>(this, OnPlayDiscLaunched);
 			Messenger.Default.Register<PlayDiscFromSongEventArgs>(this, OnPlayDiscFromSongLaunched);
 			Messenger.Default.Register<ReversePlayingEventArgs>(this, OnReversePlaying);
-
-			Messenger.Default.Register<PlayDiscEventArgs>(this, OnPlayDiscLaunched);
 		}
 
 		private async Task Load()
