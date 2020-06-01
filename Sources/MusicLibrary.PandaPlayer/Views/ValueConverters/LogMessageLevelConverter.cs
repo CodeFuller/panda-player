fc6c3046c3f7ce7316e4274886abf34d4de370b6
CodeFuller <CodeFuller@gmail.com>
2020-06-01 08:59:45 +0300

    Adjust layout of LoggerView

---
+++ Sources/MusicLibrary.PandaPlayer/Views/ValueConverters/LogMessageLevelConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using Microsoft.Extensions.Logging;
+
+namespace MusicLibrary.PandaPlayer.Views.ValueConverters
+{
+	internal class LogMessageLevelConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is LogLevel logLevel) || targetType != typeof(Brush))
+			{
+				return null;
+			}
+
+			switch (logLevel)
+			{
+				case LogLevel.Critical:
+				case LogLevel.Error:
+					return Brushes.Red;
+
+				case LogLevel.Warning:
+					return Brushes.Yellow;
+
+				case LogLevel.Debug:
+				case LogLevel.Trace:
+					return Brushes.Gray;
+
+				case LogLevel.Information:
+				default:
+					return Brushes.White;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Binding.DoNothing;
+		}
+	}
+}
