1b5c1ae329547c27729dacfb77699081ec37a26f
CodeFuller <CodeFuller@gmail.com>
2020-06-04 20:20:09 +0300

    Refactor SongMediaInfoProvider class

--- Sources/MusicLibrary.Services/Media/SongMediaInfoProvider.cs
+++ Sources/MusicLibrary.Services/Media/SongMediaInfoProvider.cs
@@ -5,24 +5,22 @@ namespace MusicLibrary.Services.Media
 {
 	public class SongMediaInfoProvider : ISongMediaInfoProvider
 	{
-		public async Task<SongMediaInfo> GetSongMediaInfo(string songFileName)
+		public Task<SongMediaInfo> GetSongMediaInfo(string songFileName)
 		{
-			return await Task.Run(() =>
-			{
-				using (TagLib.File file = TagLib.File.Create(songFileName))
-				{
-					var properties = file.Properties;
-					return new SongMediaInfo
-					{
-						Size = (int)new FileInfo(songFileName).Length,
+			using var file = TagLib.File.Create(songFileName);
 
-						// TagLib returns bitrate in Kb/s, e.g. 320 for 320 Kb/s
-						// Adjusting the value to b/s.
-						Bitrate = 1000 * properties.AudioBitrate,
-						Duration = properties.Duration,
-					};
-				}
-			});
+			var properties = file.Properties;
+			var mediaInfo = new SongMediaInfo
+			{
+				Size = (int)new FileInfo(songFileName).Length,
+
+				// TagLib returns bitrate in Kb/s, e.g. 320 for 320 Kb/s
+				// Adjusting the value to b/s.
+				Bitrate = 1000 * properties.AudioBitrate,
+				Duration = properties.Duration,
+			};
+
+			return Task.FromResult(mediaInfo);
 		}
 	}
 }
