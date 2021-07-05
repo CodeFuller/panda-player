e7f3a9aa0e762685bd61f52473fc710bb74605c4
CodeFuller <CodeFuller@gmail.com>
2021-07-05 20:55:32 +0300

    Add IT for GenresService

---
+++ tests/MusicLibrary.Services.IntegrationTests/GenresServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicLibrary.Services.Interfaces;
+
+namespace MusicLibrary.Services.IntegrationTests
+{
+	[TestClass]
+	public class GenresServiceTests : BasicServiceTests<IGenresService>
+	{
+		[TestMethod]
+		public async Task GetAllGenres_ReturnsGenresCorrectly()
+		{
+			// Arrange
+
+			var target = CreateTestTarget();
+
+			// Act
+
+			var genres = await target.GetAllGenres(CancellationToken.None);
+
+			// Assert
+
+			var referenceData = GetReferenceData();
+			var expectedGenres = new[]
+			{
+				referenceData.Genre1,
+				referenceData.Genre2,
+			};
+
+			genres.Should().BeEquivalentTo(expectedGenres);
+		}
+	}
+}
