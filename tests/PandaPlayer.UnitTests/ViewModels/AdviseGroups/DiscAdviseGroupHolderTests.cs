33d7c51748847ed0c3c3ced854818bb1e7386fb3
CodeFuller <CodeFuller@gmail.com>
2021-08-18 21:14:32 +0300

    Add UT for FolderAdviseGroupHolder and DiscAdviseGroupHolder

---
+++ tests/PandaPlayer.UnitTests/ViewModels/AdviseGroups/DiscAdviseGroupHolderTests.cs
@@ -0,0 +1,132 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PandaPlayer.Core.Models;
+using PandaPlayer.Services.Interfaces;
+using PandaPlayer.ViewModels.AdviseGroups;
+
+namespace PandaPlayer.UnitTests.ViewModels.AdviseGroups
+{
+	[TestClass]
+	public class DiscAdviseGroupHolderTests
+	{
+		[TestMethod]
+		public void InitialAdviseGroupName_IfDiscHasAlbumTitle_ReturnsDiscAlbumTitle()
+		{
+			// Arrange
+
+			var disc = new DiscModel
+			{
+				AlbumTitle = "Album Title",
+				Title = "Disc Title",
+			};
+
+			var target = new DiscAdviseGroupHolder(disc);
+
+			// Act
+
+			var initialAdviseGroupName = target.InitialAdviseGroupName;
+
+			// Assert
+
+			initialAdviseGroupName.Should().Be("Album Title");
+		}
+
+		[TestMethod]
+		public void InitialAdviseGroupName_IfDiscHasNoAlbumTitle_ReturnsDiscTitle()
+		{
+			// Arrange
+
+			var disc = new DiscModel
+			{
+				AlbumTitle = null,
+				Title = "Disc Title",
+			};
+
+			var target = new DiscAdviseGroupHolder(disc);
+
+			// Act
+
+			var initialAdviseGroupName = target.InitialAdviseGroupName;
+
+			// Assert
+
+			initialAdviseGroupName.Should().Be("Disc Title");
+		}
+
+		[TestMethod]
+		public void CurrentAdviseGroup_ReturnsDiscAdviseGroup()
+		{
+			// Arrange
+
+			var adviseGroup = new AdviseGroupModel
+			{
+				Id = new ItemId("Advise Group Id"),
+				Name = "Advise Group Name",
+			};
+
+			var disc = new DiscModel
+			{
+				AdviseGroup = adviseGroup,
+			};
+
+			var target = new DiscAdviseGroupHolder(disc);
+
+			// Act
+
+			var currentAdviseGroup = target.CurrentAdviseGroup;
+
+			// Assert
+
+			currentAdviseGroup.Should().Be(adviseGroup);
+		}
+
+		[TestMethod]
+		public async Task AssignAdviseGroup_InvokesAssignAdviseGroupForDisc()
+		{
+			// Arrange
+
+			var newAdviseGroup = new AdviseGroupModel
+			{
+				Id = new ItemId("Advise Group Id"),
+				Name = "Advise Group Name",
+			};
+
+			var disc = new DiscModel();
+
+			var adviseGroupServiceMock = new Mock<IAdviseGroupService>();
+
+			var target = new DiscAdviseGroupHolder(disc);
+
+			// Act
+
+			await target.AssignAdviseGroup(adviseGroupServiceMock.Object, newAdviseGroup, CancellationToken.None);
+
+			// Assert
+
+			adviseGroupServiceMock.Verify(x => x.AssignAdviseGroup(disc, newAdviseGroup, It.IsAny<CancellationToken>()), Times.Once);
+		}
+
+		[TestMethod]
+		public async Task RemoveAdviseGroup_InvokesRemoveAdviseGroupForDisc()
+		{
+			// Arrange
+
+			var disc = new DiscModel();
+
+			var adviseGroupServiceMock = new Mock<IAdviseGroupService>();
+
+			var target = new DiscAdviseGroupHolder(disc);
+
+			// Act
+
+			await target.RemoveAdviseGroup(adviseGroupServiceMock.Object, CancellationToken.None);
+
+			// Assert
+
+			adviseGroupServiceMock.Verify(x => x.RemoveAdviseGroup(disc, It.IsAny<CancellationToken>()), Times.Once);
+		}
+	}
+}
---
+++ tests/PandaPlayer.UnitTests/ViewModels/AdviseGroups/FolderAdviseGroupHolderTests.cs
@@ -0,0 +1,109 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PandaPlayer.Core.Models;
+using PandaPlayer.Services.Interfaces;
+using PandaPlayer.ViewModels.AdviseGroups;
+
+namespace PandaPlayer.UnitTests.ViewModels.AdviseGroups
+{
+	[TestClass]
+	public class FolderAdviseGroupHolderTests
+	{
+		[TestMethod]
+		public void InitialAdviseGroupName_ReturnsFolderName()
+		{
+			// Arrange
+
+			var folder = new ShallowFolderModel
+			{
+				Name = "Folder Name",
+			};
+
+			var target = new FolderAdviseGroupHolder(folder);
+
+			// Act
+
+			var initialAdviseGroupName = target.InitialAdviseGroupName;
+
+			// Assert
+
+			initialAdviseGroupName.Should().Be("Folder Name");
+		}
+
+		[TestMethod]
+		public void CurrentAdviseGroup_ReturnsFolderAdviseGroup()
+		{
+			// Arrange
+
+			var adviseGroup = new AdviseGroupModel
+			{
+				Id = new ItemId("Advise Group Id"),
+				Name = "Advise Group Name",
+			};
+
+			var folder = new ShallowFolderModel
+			{
+				AdviseGroup = adviseGroup,
+			};
+
+			var target = new FolderAdviseGroupHolder(folder);
+
+			// Act
+
+			var currentAdviseGroup = target.CurrentAdviseGroup;
+
+			// Assert
+
+			currentAdviseGroup.Should().Be(adviseGroup);
+		}
+
+		[TestMethod]
+		public async Task AssignAdviseGroup_InvokesAssignAdviseGroupForFolder()
+		{
+			// Arrange
+
+			var newAdviseGroup = new AdviseGroupModel
+			{
+				Id = new ItemId("Advise Group Id"),
+				Name = "Advise Group Name",
+			};
+
+			var folder = new ShallowFolderModel();
+
+			var adviseGroupServiceMock = new Mock<IAdviseGroupService>();
+
+			var target = new FolderAdviseGroupHolder(folder);
+
+			// Act
+
+			await target.AssignAdviseGroup(adviseGroupServiceMock.Object, newAdviseGroup, CancellationToken.None);
+
+			// Assert
+
+			adviseGroupServiceMock.Verify(x => x.AssignAdviseGroup(folder, newAdviseGroup, It.IsAny<CancellationToken>()), Times.Once);
+		}
+
+		[TestMethod]
+		public async Task RemoveAdviseGroup_InvokesRemoveAdviseGroupForFolder()
+		{
+			// Arrange
+
+			var folder = new ShallowFolderModel();
+
+			var adviseGroupServiceMock = new Mock<IAdviseGroupService>();
+
+			var target = new FolderAdviseGroupHolder(folder);
+
+			// Act
+
+			await target.RemoveAdviseGroup(adviseGroupServiceMock.Object, CancellationToken.None);
+
+			// Assert
+
+			adviseGroupServiceMock.Verify(x => x.RemoveAdviseGroup(folder, It.IsAny<CancellationToken>()), Times.Once);
+		}
+	}
+}
